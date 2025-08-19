584509ff841b1fb2170a0bdbec187c3ef4d489e0
farhansm01 <farhansadiq2021@gmail.com>
2025-08-20 00:11:10 +0600

    changed greeting method!

--- Hello.java
+++ Hello.java
@@ -1,10 +1,11 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Vs code is awsome...");
-        greetings("Farhan Sadiq");
+        
+        greetings("Github")
     }
 
     public static void greetings(String name){
-        System.out.println("Welcome " + name);
+        System.out.println("Hello " + name);
     }
 }
