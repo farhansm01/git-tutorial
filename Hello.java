0bb3dc58de218f0df6bdfbcc40e3933ae573f93d
farhansm01 <farhansadiq2021@gmail.com>
2025-08-19 22:57:59 +0600

    added a new method called greetings

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
-    public static void main([string] args){
+    public static void main(String[] args){
         System.out.println("Vs code is awsome...");
+        greetings("Farhan Sadiq");
+    }
+
+    public static void greetings(String name){
+        System.out.println("Welcome " + name);
     }
 }
