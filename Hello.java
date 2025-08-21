d44e733fee25c90fffd83a171739ebaf2e4b428d
farhansm01 <farhansadiq2021@gmail.com>
2025-08-21 11:36:36 +0600

    back to merged version of brA

--- Hello.java
+++ Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
-    public static void main([string] args){
+    public static void main(String[] args){
         System.out.println("Vs code is awsome...");
+        
+        display();
+    }
+
+    public static void dispaly(){
+        System.out.println("Hello World!");
+    }
+
+    public static int sqr(int x){
+        return x*x;
     }
 }
