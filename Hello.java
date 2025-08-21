6688e1a34dc2dd23453e1052b9162e7f1b645765
farhansm01 <farhansadiq2021@gmail.com>
2025-08-21 11:16:46 +0600

    created branchC and removed all the previous methods and added new method called message()

--- Hello.java
+++ Hello.java
@@ -2,14 +2,11 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Vs code is awsome...");
         
-        display();
+        
     }
 
-    public static void dispaly(){
-        System.out.println("Hello World!");
+    public static void message(){
+        System.out.println("Hooray!! now we are able to contribute to open source!");
     }
 
-    public static int sqr(int x){
-        return x*x;
-    }
 }
